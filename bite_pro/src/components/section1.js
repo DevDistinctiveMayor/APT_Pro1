0abe96e1419b79c26bad986af6216195f83d5146
Barugok Martin Allahmagani <barughokmartin@gmail.com>
2024-06-14 11:18:15 +0100

    Just a test change

---
+++ bite_pro/src/components/section1.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function section1() {
+  return (
+    <div>
+        <>
+            My first Comment
+        </h1>
+
+
+
+
+    </div>
+  )
+}
+
+export default section1
